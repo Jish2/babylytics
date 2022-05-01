5ee88ce4ded9279760fe5e874e00e38ad7b8a3e8
Joshua Goon <j.j.jappiecraft@gmail.com>
2022-04-30 21:25:06 -0500

    display data

---
+++ frontend/src/Data.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Data = () => {
+	return (
+		<div>
+			<h1 className="display-1">Based on the data...</h1>
+			<h3 className="text-success">The fetus is healthy</h3>
+		</div>
+	);
+};
+
+export default Data;
